efef252bed3b1cc89b04ebc2c9123f81109720bf
Pria Admaja <priaadmaja@gmail.com>
2025-08-19 22:17:13 +0700

    first init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<section
+	class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0"
+>
+	<section class="lg:flex lg:justify-between lg:gap-4">
+		<header>
+			<h1 class="text-4xl font-bold">Pria Admaja</h1>
+		</header>
+	</section>
+</section>
