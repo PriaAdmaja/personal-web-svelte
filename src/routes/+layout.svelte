aa49d4b59d802169627ac15b792b3b0f8be41bec
Pria Admaja <priaadmaja@gmail.com>
2025-08-20 08:26:11 +0700

    Add roboto font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 {@render children?.()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,34 @@
+<script>
+	import { Heading, P } from 'flowbite-svelte';
+	import { GithubSolid, LinkedinSolid } from 'flowbite-svelte-icons';
+</script>
+
 <section
-	class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0"
+	class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-roboto md:px-12 md:py-16 lg:py-0"
 >
 	<section class="lg:flex lg:justify-between lg:gap-4">
-		<header>
-			<h1 class="text-4xl font-bold">Pria Admaja</h1>
+		<header
+			class="lg:sticky lg:top-0 lg:flex lg:h-screen lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24"
+		>
+			<div>
+				<Heading tag="h1" class="text-4xl font-bold tracking-tight"
+					><a href="/">Pria Admaja</a></Heading
+				>
+				<Heading tag="h2" class="mt-3 text-lg font-medium tracking-tight sm:text-xl"
+					>Front End Engineer</Heading
+				>
+				<P class="mt-4 max-w-xs leading-normal text-pretty"
+					>I build fast, accessible, and scalable front-end applications</P
+				>
+			</div>
+			<div class="flex gap-4">
+				<a href="https://github.com/PriaAdmaja" target="_blank" rel="noopener noreferrer">
+					<GithubSolid class="size-6" />
+				</a>
+				<a href="https://www.linkedin.com/in/priaadmaja/" target="_blank" rel="noopener noreferrer">
+					<LinkedinSolid class="size-6" />
+				</a>
+			</div>
 		</header>
 	</section>
 </section>
