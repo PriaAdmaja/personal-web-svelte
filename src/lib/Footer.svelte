3d5b7926df508e553d529eecd65a771ad7828d86
Pria Admaja <priaadmaja@gmail.com>
2025-09-19 08:39:12 +0700

    Update Footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { DarkMode, P } from 'flowbite-svelte';
+	import ExternalLink from './ExternalLink.svelte';
 </script>
 
 <section class="flex items-center justify-between">
-	<P class="text-sm text-pretty">© 2024 Pria Admaja.</P>
+	<P class="text-sm">&copy; {new Date().getFullYear()} Pria Admaja</P>
+
 	<DarkMode class="cursor-pointer" />
 </section>
