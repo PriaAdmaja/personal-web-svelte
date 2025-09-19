1176aa8843ad920d2504aa81e48b023ca23e7e7b
Pria Admaja <priaadmaja@gmail.com>
2025-09-19 12:38:41 +0700

    Update header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,11 @@
 		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
 		rel="stylesheet"
 	/>
+	<title>Pria Admaja</title>
+	<meta
+		name="description"
+		content="Hi, I’m Pria Admaja. A Frontend Engineer passionate about building modern web apps with React, Next.js, and Tailwind CSS."
+	/>
 </svelte:head>
 
 {@render children?.()}
