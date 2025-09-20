9d801c608889ab56db5ad01227e494fcdd185d6a
Pria Admaja <priaadmaja@gmail.com>
2025-09-20 13:48:23 +0700

    edit favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={favicon} type="image/svg+xml"/>
 	<link
 		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
 		rel="stylesheet"
